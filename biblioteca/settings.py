38d1aae5f93780cb99227c0ab7825f008702b6b1
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-04-06 20:10:43 -0300

    corrigi a instalação da app no settings.py

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'livros'
+    'livros.apps.LivrosConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
