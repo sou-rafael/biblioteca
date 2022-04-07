3f1c51b25959553b002c4c50d40e7bd531f6dd25
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-04-07 11:20:27 -0300

    Acrescentei a listagem na base.html, defini os links para editar e deletar. Prox configurar as views e ativar as respect htmls

--- livros/views.py
+++ livros/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Livros
 
 def index(request):
-    return render(request, 'livros/base.html')
+    '''Exibe tanto a pagina incial quanto ja faz uma listagem dos itens'''
+    livros = Livros.objects.all()
+    return render(request, 'livros/base.html', {'livros': livros})
+
+def editar(request, id):
+
+
+def deletar(request, id):
